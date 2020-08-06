f33dc112440a294829634ef09930d9ddbdc95da3
Queenissa <atacador.quency@gmail.com>
2020-08-06 12:53:38 +0800

    Initial Commit

---
+++ src/views/components/coreSkills.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="wrapper">
+    <div class="space-50"></div>
+
+    <!-- Progress/Pagination -->
+    <div id="progress-pagination">
+      <div class="md-layout">
+
+        <div class="md-layout-item md-size-50 md-xsmall-size-100">
+          <div class="title">
+            <h3>Skills</h3>
+          </div>
+         <!-- <img src="@assets/img/html.png"> -->
+        </div>
+
+        <div class="md-layout-item md-size-50 md-xsmall-size-100">
+          <div class="title">
+            <h3>Rate</h3>
+          </div>
+          <md-progress-bar
+            class="md-primary"
+            md-mode="buffer"
+            :md-value="amount"
+          ></md-progress-bar>
+          <md-progress-bar
+            class="md-info"
+            md-mode="buffer"
+            :md-value="amount2"
+          ></md-progress-bar>
+          <md-progress-bar
+            class="md-warning"
+            md-mode="buffer"
+            :md-value="buffer"
+            :md-buffer="buffer"
+          ></md-progress-bar>
+        </div>
+      </div>
+    </div>
+    <!-- end progress/pagination -->
+
+    <div class="space-50"></div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      amount: 30,
+      amount2: 60,
+      buffer: 40
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.vertical-center {
+  display: flex;
+  align-items: center;
+}
+.flex-column {
+  display: flex;
+  flex-direction: column;
+}
+.md-checkbox,
+.md-radio {
+  display: flex;
+  margin: 0;
+  margin-bottom: 0.5rem;
+}
+</style>
---
+++ src/views/homepage.vue
@@ -0,0 +1,168 @@
+<template>
+  <div class="wrapper">
+    <parallax id="header-page" class="page-header header-filter" :style="headerStyle">
+      <div class="md-layout">
+        <div class="md-layout-item">
+          <div class="image-wrapper">
+            <div class="brand">
+              <!-- <h1>Mary Jane Cajes Paller</h1> -->
+              <h3>"New idea can be a great innovation"</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </parallax>
+    <div class="main main-raised">
+        <div class="container">
+          <aboutMe/>
+        </div>
+
+      <div class="section section-basic">
+        <div class="container">
+          <div class="title">
+            <h2>Core Skills</h2>
+          </div>
+          <coreSkills></coreSkills>
+        </div>
+      </div>
+
+      <div class="section section-examples">
+        <div class="container-fluid text-center">
+          <div class="title">
+            <h3>Experience</h3>
+          </div>
+          <div class="md-layout">
+            <div class="md-layout-item">
+              <a href="#/landing" target="_blank">
+                <img
+                  :src="landing"
+                  alt="Rounded Image"
+                  class="img-raised rounded img-fluid"
+                />
+              </a>
+              <md-button href="#/landing" class="md-simple md-success md-lg"
+                >View Landing Page</md-button
+              >
+            </div>
+            <div class="md-layout-item">
+              <a href="#/profile" target="_blank">
+                <img
+                  :src="profile"
+                  alt="Rounded Image"
+                  class="img-raised rounded img-fluid"
+                />
+              </a>
+              <md-button href="#/profile" class="md-simple md-success md-lg"
+                >View Profile Page</md-button
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="section">
+        <div class="container text-center">
+          <div class="md-layout">
+            <div
+              class="md-layout-item md-size-66 md-xsmall-size-100 ml-auto mr-auto text-center"
+            >
+              <h2>Contact Me</h2>
+                <div>
+                    <h3><i class="fas fa-envelope"></i> Email</h3>
+                    <p>maryjane.paller@student.passerellesnumeriques.org</p>
+                </div>
+                <div>
+                    <h3><i class="fas fa-phone-square"></i> Phone Number</h3>
+                    <p>09123456789</p>
+                </div>
+                <div>
+                    <h3><i class="fab fa-facebook"></i> Facebook Account</h3>
+                    <p>Mary Jane Cajes Paller</p>
+                </div>
+                <!-- <md-button
+                    href="javascript:void(0)"
+                    class="md-just-icon md-simple md-envelope"
+                    ><i class="fas fa-envelope"></i
+                  ></md-button> -->
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="section section-signup page-header" :style="signupImage">
+        <div class="container">
+          <div class="md-layout">
+           
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import aboutMe from "./components/aboutMe";
+import coreSkills from "./components/coreSkills";
+
+export default {
+  components: {
+    aboutMe,
+    coreSkills
+  },
+  name: "index",
+  bodyClass: "index-page",
+  props: {
+    image: {
+      type: String,
+      default: require("@/assets/img/vue-mk-header.jpg")
+    },
+    signup: {
+      type: String,
+      default: require("@/assets/img/city.jpg")
+    },
+    landing: {
+      type: String,
+      default: require("@/assets/img/landing.jpg")
+    },
+    profile: {
+      type: String,
+      default: require("@/assets/img/profile.jpg")
+    }
+  },
+  data() {
+    return {
+      firstname: null,
+      email: null,
+      password: null,
+      leafShow: false
+    };
+  },
+  computed: {
+    headerStyle() {
+      return {
+        backgroundImage: `url(${this.image})`
+      };
+    },
+    signupImage() {
+      return {
+        backgroundImage: `url(${this.signup})`
+      };
+    }
+  }
+};
+</script>
+<style lang="scss">
+.section-download {
+  .md-button + .md-button {
+    margin-left: 5px;
+  }
+}
+
+@media all and (min-width: 991px) {
+  .btn-container {
+    display: flex;
+  }
+}
+#header-page{
+  height: 700px;
+}
+</style>
